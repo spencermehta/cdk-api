0302af7719d05868d472ac59e1c8faf99ed787c4
Spencer Mehta <spencermehta@gmail.com>
2022-01-13 11:29:07 +0000

    Add endpoints to cfn output

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps CfnOutput} from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
@@ -6,6 +6,9 @@ import { HitCounter } from "./hitcounter";
 import { TableViewer } from "cdk-dynamo-table-viewer";
 
 export class CdkWorkshopStack extends Stack {
+  public readonly hcViewerUrl: CfnOutput;
+  public readonly hcEndpiont: CfnOutput;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -19,14 +22,22 @@ export class CdkWorkshopStack extends Stack {
       downstream: hello,
     });
 
-    new apigw.LambdaRestApi(this, "Endpoint", {
+    const gateway = new apigw.LambdaRestApi(this, "Endpoint", {
       handler: helloWithCounter.handler,
     });
 
-    new TableViewer(this, "ViewHitCounter", {
+    const tv = new TableViewer(this, "ViewHitCounter", {
       title: "Hello Hits",
       table: helloWithCounter.table,
       sortBy: "-hits",
+    });
+
+    this.hcEndpiont = new CfnOutput(this, "GatewayUrl", {
+      value: gateway.url,
+    });
+
+    this.hcViewerUrl = new CfnOutput(this, "TableViewerUrl", {
+      value: tv.endpoint,
     });
   }
 }
